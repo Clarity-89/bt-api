c811ca742b6a9ca31ae5ac0d4dfb60c33cf77538
Oleksandr Khomenko <homes89@ukr.net>
2019-03-21 21:12:21 +0200

    Hops migrations

--- migrations/20190317151644_create_hops.js
+++ migrations/20190317151644_create_hops.js
@@ -1,12 +1,18 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable("hops", table => {
-      table.increments("id").primary();
-      table.string("name");
-      table.string("description");
-      table.string("alpha_acid_composition");
-      table.integer("substitutes").references("hops.id");
-    })
+    knex.schema
+      .createTable("hops", table => {
+        table.increments("id").primary();
+        table.string("name").notNullable();
+        table.string("description");
+        table.string("alpha_acid_composition");
+        table.string("beta_acid_composition");
+        table.string("origin");
+        table.enu("purpose", ["bittering", "aroma", "bittering & aroma"]);
+        table.integer("substitutes").references("hops.id");
+        table.string("beer_style");
+      })
+      .catch(error => console.log("error migrating hops", error))
   ]);
 };
 
