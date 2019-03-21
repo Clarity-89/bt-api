fd0a438bcae4992ad99dc64843ddf32afd9d7e31
Oleksandr Khomenko <homes89@ukr.net>
2019-03-21 22:00:10 +0200

    Add down countries migration

--- migrations/20190321215041_countries.js
+++ migrations/20190321215041_countries.js
@@ -10,4 +10,6 @@ exports.up = function(knex, Promise) {
   ]);
 };
 
-exports.down = function(knex, Promise) {};
+exports.down = function(knex, Promise) {
+  return Promise.all([knex.schema.dropTable("countries")]);
+};
