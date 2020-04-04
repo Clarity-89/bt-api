8122ec8a86235776312e0722bb21d53c09c17214
Clarity-89 <homes89@ukr.net>
2020-04-04 16:24:27 +0300

    Remove Promise from seeds

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const schema = makeExecutableSchema({
 const app = restify.createServer();
 const cors = corsMiddleware({ origins: ["http://localhost:3000"] });
 
-const port = 3001;
+const port = 3002;
 app.pre(cors.preflight);
 app.use(cors.actual);
 
